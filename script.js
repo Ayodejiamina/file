7c050d2c53d21a80978aedec93c8926c122762ad
Ayodeji Amina Ayomide <ayodejiamina@gmail.com>
2022-08-29 11:08:50 +0100

    form

--- script.js
+++ script.js
@@ -11,17 +11,20 @@ let proerror = document.getElementById("profile-error")
 
 let formsec= document.forms['formsec'];
 
-formsec.addEventListener("submit", (event) =>{
-    event.preventDefault();
+formsec.addEventListener("submit", (e) =>{
+   e.preventDefault();
+
+
 if(namesec.value === ""){
     namerror.innerHTML="Name is required"
     namerror.style.color = "red"
+}else{
+    namerror.style.display = "none"
 }
 if(emailsec.value.includes('@') && emailsec.value.includes('.')){
-    emailerror.innerHTML = "👌😘correct."
-    emailerror.style.color = "green"
 }else if(emailsec.value ===""){
     emailerror.innerHTML="Please input your email address"
+    emailerror.style.color = "red"
 }else{
     emailerror.innerHTML="Invalid Email Address"
     emailerror.style.color = "red"
@@ -29,40 +32,53 @@ if(emailsec.value.includes('@') && emailsec.value.includes('.')){
 
 if(phone.value == "" || phone.value ==0){
     numerror.innerHTML = "Your contact is required"
-}
-else if (phone.value.startsWith('080')
+}else if (phone.value.startsWith('080')
 ||phone.value.startsWith('091') 
 ||phone.value.startsWith('090') 
 ||phone.value.startsWith('081')
 ||phone.value.startsWith('070')){
-    numerror.innerHTML =  "👌😘correct."
-    numerror.style.color = "green"
-    
-} 
-else if(phone.value.length >=12){
-    numerror.innerHTML = "invalid number"
-}else{
- numerror.innerHTML="Invalid Phone-number"
+} else{
+    numerror.innerHTML="Invalid Phone-number"
+   
+ 
+}
+ if(phone.value.length > 11){
+    numerror.innerHTML = "Phone number is greater than 11"
+}
 
-} 
 if(!namerror.value || !emailerror.value || !numerror.value || !proerror.value){
     document.getElementById("Username").innerHTML=namesec.value
     document.getElementById("Useremail").innerHTML=emailsec.value
     document.getElementById("User-number").innerHTML=phone.value
     document.getElementById("User-profile").innerHTML=file.value
+
 }
-if(file.value== ""|| file.value == 0){
+if (!(phone.value.startsWith('080')
+||phone.value.startsWith('091') 
+||phone.value.startsWith('090')
+||phone.value.startsWith('081') 
+||phone.value.startsWith('070'))){
+    document.getElementById("User-number").style.display = "none"
+    
+} 
+if(!(emailsec.value.includes('@') && emailsec.value.includes('.'))){
+    document.getElementById("Useremail").style.display = "none"
+}
+    if(file.value== ""|| file.value == 0){
     proerror.innerHTML = "please upload a file"
     proerror.style.color = "red"
 }else if(file.files[0].size > (1024*1024*1) ){
     proerror.innerHTML ="image greater than 1MB"
     proerror.style.color = "red"
-}
-
- 
+}else{
+    proerror.style.display="none"
+} 
 
 
 })
+
+
+
 
 
 
